2519c26f813dec3d79a2cb4cda965e886ef3f6dd
1114170770@qq.com <1114170770@qq.com>
2022-08-16 23:01:02 +0800

    build

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
+import { CatsModule } from './cats/cats.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
-  providers: [AppService],
+	imports: [CatsModule, TypeOrmModule.forRoot({
+		type: 'mysql',
+		host: '47.97.11.78',
+		port: 3306,
+		username: 'root',
+		password: '_doA4dcB8gYG3',
+		database: 'error_log',
+	})],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { ListAllEntities, CreateCatDto, UpdateCatDto } from './dto/dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interface/cat.interface';
 
 @Controller('cats')
 export class CatsController {
-    @Get('all')
-    findAll(): String {
-        return 'This action returns all cats'
+
+    constructor(private catsService: CatsService) {}
+
+    @Get()
+    async findAll(@Query() query: ListAllEntities): Promise<Cat[]> {
+        return this.catsService.findAll()
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return `this action returns #${id} cat`
+    }
+
+    @Post()
+    async create(@Body() createCatDto: CreateCatDto) {
+        this.catsService.create(createCatDto)
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+        return `this action updates a #${id} cat`
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return `this action removes a #${id} cat`
     }
 }
